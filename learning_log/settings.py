7538285fbf0dab349a53ce6d78e4e47a6a030579
Anthony <anthony@iMac-Anthony.local>
2020-06-12 02:04:26 +0300

    try4

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -135,7 +135,7 @@ if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     ALLOWED_HOSTS = ['*']
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    STATIC_ROOT = '/static/'
     '''STATICFILES_DIRS = (
            os.path.join(BASE_DIR, 'static'),
        )'''
